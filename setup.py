3169a83df06aef730d9ed331e6a2e11cf53a3a52
Satyapratap Ahirwar <satyapratap167@gmail.com>
2025-05-10 17:08:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "pyfiglet>=1.0,<2.0",
-        "python-barcode>=1.0,<2.0",
+        "python-barcode>=0.15",
         "pyqrcode>=1.2,<2.0",
         "phonenumbers>=8.13,<9.0",
         "requests>=2.31,<3.0",
